d042050c08ab8d058cc28fbe6acca58755991037
larsko <larsko@477b24dd-e91e-0410-aa49-980f3df0729b>
2008-12-12 13:09:24 +0000

    checking for return values in build scripts so that hudson knows when something failed
    git-svn-id: svn+ssh://keith.cs.st-and.ac.uk/mnt/raid/svn/repositories/minion-private/public/trunk@2100 477b24dd-e91e-0410-aa49-980f3df0729b

--- translator/build.sh
+++ translator/build.sh
@@ -3,18 +3,12 @@
 #    installed on your machine in order to build the parser and lexer
 cd src/translator/essencePrimeParser/
 echo "### Generating EssencePrimeParser"
-./build_parser.sh
-cd ..
-cd ..
-cd ..
+./build_parser.sh || exit 1 
+cd ../../..
 echo "### Generating XCSP functionals parser"
 cd src/translator/xcsp2ep/mapper/functionalsParser/
-./build_parser.sh
-cd ..
-cd ..
-cd ..
-cd ..
+./build_parser.sh || exit 1 
+cd ../../../..
 echo "### Compiling conjureEssenceSpecification, parser and Minion-part"
-javac -classpath translator/essencePrimeParser/java-cup-11a.jar:. -d ../ translator/essencePrimeParser/*.java translator/expression/*.java translator/normaliser/*.java translator/tailor/*.java  translator/gui/*.java translator/solver/*.java translator/tailor/minion/*.java translator/tailor/gecode/*.java translator/xcsp2ep/parser/*.java translator/xcsp2ep/mapper/functionalsParser/*.java translator/xcsp2ep/mapper/*.java translator/xcsp2ep/*.java  translator/*.java
-cd ..
-cd ..
+javac -classpath translator/essencePrimeParser/java-cup-11a.jar:. -d ../ translator/essencePrimeParser/*.java translator/expression/*.java translator/normaliser/*.java translator/tailor/*.java  translator/gui/*.java translator/solver/*.java translator/tailor/minion/*.java translator/tailor/gecode/*.java translator/xcsp2ep/parser/*.java translator/xcsp2ep/mapper/functionalsParser/*.java translator/xcsp2ep/mapper/*.java translator/xcsp2ep/*.java  translator/*.java || exit 1  
+cd ../..
